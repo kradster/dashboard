561d66c76f81143eb829e039d9a8621dea897589
vinod karadiya <vinodkaradiya@gmail.com>
2018-04-22 13:23:34 +0530

    basic added

---
+++ js/charts-js/card_chart_1.js
@@ -0,0 +1,72 @@
+var ctx = document.getElementById("myCardChart1").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["", "", "", "", "", ""],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)'
+                
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    display:false
+                },
+                gridLines: {
+                    color: "rgba(255,255,255,0.0)",
+                    zeroLineColor: "rgba(254,254,254, 0.0)"
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    display:false
+                },
+                gridLines: {
+                    color: "rgba(255,255,255,0.0)",
+                    zeroLineColor: "rgba(254,254,254, 0.0)"
+                }
+            }]
+        },
+        legend: {
+            display:false,
+            labels: {
+                
+            },
+            title: {
+                display: true,
+                text: 'Custom Chart Title'
+            },
+        },
+        layout: {
+            padding: {
+                left:0,
+                right: 10,
+                top: 0,
+                bottom: 0
+            }
+        },
+        tooltips:{
+            enabled:false
+        }
+    }
+});
